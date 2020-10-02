3799e469445bbe40c400d838f6361350ed5217ad
afenk <artyom.antnv@gmail.com>
2020-10-02 13:09:35 +0300

    validate_config.py fix

--- validate_config.py
+++ validate_config.py
@@ -7,6 +7,9 @@ def check_config(config: type(Config)):
         error_message += 'Ошибка!\nНе указан путь к файлу для чтения.\nУкажите путь в переменной PATH_TO_EXCEL_FILE в config.py\n'
     if config.PATH_TO_SAVE == '':
         error_message += 'Ошибка!\nНе указан путь к файлу для сохранения.\nУкажите путь в переменной PATH_TO_SAVE в config.py\n'
+    if error_message:
+        print(error_message)
+        return False
     if config.PATH_TO_EXCEL_FILE.__contains__(' ') or config.PATH_TO_SAVE.__contains__(' '):
         error_message += 'Ошибка!\nПуть не может содержать пробела\nПроверьте переменные пути в config.py\n'
     if not (config.PATH_TO_EXCEL_FILE.split('.')[1] == 'xlsx' and config.PATH_TO_SAVE.split('.')[1] == 'xlsx'):
